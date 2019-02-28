4a01ecf371451d69869417172a2706502b965c26
Miguel Palau Zarza <mpalauzarza@gmail.com>
2019-02-28 14:16:54 -0600

    more random pics

--- example/src/App.js
+++ example/src/App.js
@@ -4,9 +4,9 @@ import Lightbox from '@cratejoy/lightbox'
 
 const imgs = [
   'https://picsum.photos/400/400',
-  'https://picsum.photos/400/400',
-  'https://picsum.photos/400/400',
-  'https://picsum.photos/400/400'
+  'https://picsum.photos/200/450',
+  'https://picsum.photos/500/500',
+  'https://picsum.photos/600/300'
 ]
 
 const Example = () => (
