0da4c22118b2646708c444387c95350931e48f38
Miguel Palau Zarza <mpalauzarza@gmail.com>
2019-03-01 14:21:58 -0600

    better icons

--- src/icons/CaretRight/CaretRight.js
+++ src/icons/CaretRight/CaretRight.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-const CaretRight = ({ fill = '#fff', ...props }) => (
-  <svg width='11' height='16' viewBox='0 0 11 16' fill={fill} {...props}>
-    <polygon points='1302.143 617 1299 620.2 1303.714 625 1299 629.8 1302.143 633 1310 625' transform='translate(-1299 -617)' />
+const CaretRight = props => (
+  <svg width='11' height='16' viewBox='0 0 11 16' {...props}>
+    <polygon points='3.14 0 0 3.2 4.71 8 0 12.8 3.14 16 11 8 3.14 0' />
   </svg>
 )
-
-CaretRight.propTypes = {
-  fill: PropTypes.string
-}
 
 export default CaretRight
