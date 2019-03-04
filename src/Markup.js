3b5f2065e5d38de24890146a3e726ad2c00deaa9
Miguel Palau Zarza <mpalauzarza@gmail.com>
2019-03-04 10:35:02 -0600

    lighter markup

--- src/Markup.js
+++ src/Markup.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Portal from 'react-minimalist-portal'
 
@@ -10,34 +10,29 @@ import {
   Overlay
 } from './components'
 
-class LightboxMarkup extends PureComponent {
-  render () {
-    if (!this.props.showPortal) return false
+const LightboxMarkup = ({
+  showPortal, src,
+  toggleOpen, index
+}) => {
+  if (!showPortal) return false
+  const currentImg = src[index]
+  return (
+    <Portal>
+      <Overlay dismiss={toggleOpen}>
+        <Image src={currentImg} show />
+      </Overlay>
+    </Portal>
+  )
+}
 
-    const {
-      src,
-      index
-    } = this.props
-
-    const currentImg = src[index]
-    return (
-      <Portal>
-        <Overlay>
-          <Image src={currentImg} show />
-        </Overlay>
-      </Portal>
-    )
-  }
-
-  static propTypes = {
-    showPortal: PropTypes.bool.isRequired,
-    src: PropTypes.arrayOf(
-      PropTypes.string
-    ).isRequired,
-    // mouseIdle: PropTypes.bool,
-    // triggerOpen: PropTypes.func,
-    index: PropTypes.number
-  }
+LightboxMarkup.propTypes = {
+  showPortal: PropTypes.bool.isRequired,
+  src: PropTypes.arrayOf(
+    PropTypes.string
+  ).isRequired,
+  // mouseIdle: PropTypes.bool,
+  toggleOpen: PropTypes.func,
+  index: PropTypes.number
 }
 
 export default LightboxMarkup
