4c7b10d1c070a5bb0b2982ec399f6bb5547f520f
Miguel Palau Zarza <mpalauzarza@gmail.com>
2019-03-07 12:49:50 -0600

    pretty button buttons as well

--- src/styles.js
+++ src/styles.js
@@ -14,17 +14,20 @@ export const Btn = styled.button`
   top: 0;
   ${({left}) => left ? 'left' : 'right'}: 0;
   display: flex;
-  align-items: center;
-  justify-content: center;
   transition: opacity 0.2s;
   opacity: 0;
+  box-shadow: none;
   background-image: linear-gradient(
     to ${({left}) => left ? 'right' : 'left'},
     rgba(0, 0, 0, 0.7) -20%, rgba(0, 0, 0, 0.01)
   );
-  box-shadow: none;
 
   &:hover {
     opacity: 1;
+  }
+
+  svg {
+    fill: currentColor;
+    margin: auto;
   }
 `
