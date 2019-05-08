56098d134aba25ce6cafa3de6de5c2cba0b34f5c
Howie Benefiel <howie@cratejoy.com>
2019-05-08 11:32:32 -0500

    Remove button shadows on mobile (#5)
    * Remove background shadow on mobile
    
    * adding shadowy arrows

--- src/icons/CaretLeft/CaretLeft.js
+++ src/icons/CaretLeft/CaretLeft.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import styled, {css} from 'styled-components'
+import PropTypes from 'prop-types'
 
-const CaretLeft = props => (
-  <svg width='11' height='16' viewBox='0 0 11 16' {...props}>
-    <polygon points='0 8 7.86 16 11 12.8 6.29 8 11 3.2 7.86 0 0 8' />
+const Caret = ({ className }) => (
+  <svg width='15' height='20' viewBox='0 0 15 20' className={className}>
+    <defs>
+      <path id='b' d='M5 12l7.9 8 3.1-3.2-4.7-4.8L16 7.2 12.9 4z' />
+      <filter id='a' width='154.5%' height='137.5%' x='-27.3%' y='-18.8%' filterUnits='objectBoundingBox'>
+        <feOffset in='SourceAlpha' result='shadowOffsetOuter1' />
+        <feGaussianBlur in='shadowOffsetOuter1' result='shadowBlurOuter1' stdDeviation='1' />
+        <feColorMatrix in='shadowBlurOuter1' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.86175153 0' />
+      </filter>
+    </defs>
+    <g fill='none' transform='translate(-3 -2)'>
+      <use fill='#000' filter='url(#a)' xlinkHref='#b' />
+      <use fill='#FFF' xlinkHref='#b' />
+    </g>
   </svg>
 )
+
+Caret.propTypes = {
+  className: PropTypes.string
+}
+
+const CaretLeft = styled(Caret)`
+  ${({rotated}) => rotated && css`
+    transform: rotate(180deg);
+  `}
+`
 
 export default CaretLeft
